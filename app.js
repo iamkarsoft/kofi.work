4a97fcf002c22b8cb2b34296ed22994cc8978ab9
Kofi Ramos Amoussou <karsoft92@gmail.com>
2019-05-08 22:21:00 +0000

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require('express');
+var pug = require('pug');
+
+var app = express();
+
+fs = require('fs');
+
+
+port = parseInt(process.env.PORT, 10) || 8686;
+
+
+app.use('/static/',express.static(__dirname + '/src/public'));
+
+app.set('view engine','pug')
+
+app.set('views',__dirname + '/src/templates');
+
+
+app.gget('', function(req,res){
+	var path = req.path;
+	res.render('index')
+
+})
