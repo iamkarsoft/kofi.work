fffc3c71e1250c39d6fdfe9721e57c41bace5ec9
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-07-09 22:17:57 +0000

    update lip link

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,7 +6,7 @@
       <div class="lg:flex items-center">
         <a href="" class="m-4">Home</a>
         <a href="" class="m-4">Portfolio</a>
-        <a href="http://kofiramos.work" target="_blank" class="m-4">LIP</a>
+        <a href="https://blog.kofi.work" target="_blank" class="m-4">LIP</a>
       </div>
     </nav>
 </template>
