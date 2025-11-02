e99549802d93adfc850528300311a0e6476ccca1
Kofi Ramos <karsoft92@gmail.com>
2025-11-02 15:26:59 +0000

    Improvements

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -14,7 +12,7 @@ export default {
 @import "./assets/css/animate.min.css";
 @import url("https://fonts.googleapis.com/css?family=Abril+Fatface|Montserrat&display=swap");
 
-#app {
+body {
   font-family: , Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
