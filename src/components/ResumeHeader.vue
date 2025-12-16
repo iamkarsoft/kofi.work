3d1e2c0550509ca48f4f9ec473783e507178d823
Kofi Ramos <karsoft92@gmail.com>
2025-12-16 17:12:49 +0000

    Remove link page and clean up

--- src/components/ResumeHeader.vue
+++ src/components/ResumeHeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <header class="w-full  flex lg:justify-between p-4">
+  <header class="w-full  lg:flex lg:justify-between p-4">
     <section>
       <h1 class="text-4xl lg:text-6xl text-orange font-extrabold">Kofi Romain Amoussou</h1>
     </section>
 
     <section class="flex px-4 items-center">
-        <Social href="https://github.com/iamkarsoft" social="github" name="github" class="  fab fa-github fa-2x" target="_blank"></Social>
+        <Social href="https://github.com/iamkarsoft" social="github" name="github" class="fab fa-github fa-2x" target="_blank"></Social>
         <a href="mailto:karsoft92@gmail.com" class="mx-2 mt-4" target="_blank"><i class="fas fa-envelope fa-2x"></i></a>
     </section>
 
